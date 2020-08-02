22365977aebdc750e0bcc5f187176ddd4a3c17a7
taka0san <takashun722@gmail.com>
2020-08-03 07:24:36 +0900

    profile

--- sample.rb
+++ sample.rb
@@ -1,1 +1,20 @@
 puts "hallo world"
+
+profile = {}
+profile[:name] = "太郎"
+profile[:age] = "26"
+profile[:address] = "高知"
+puts profile
+
+profiles = []
+profiles << profile
+
+profile2 = {}
+profile2[:name] = "二郎"
+profile2[:age] = "27"
+profile2[:address] = "愛媛"
+puts profile2
+
+profiles << profile2
+
+puts profiles
