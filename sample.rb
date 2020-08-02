9b0056467799154717ef5231a70476b8755a2b78
taka0san <takashun722@gmail.com>
2020-08-03 07:40:51 +0900

    Update sample.rb

--- sample.rb
+++ sample.rb
@@ -1,20 +1,34 @@
-puts "hallo world"
-
 profile = {}
 profile[:name] = "太郎"
-profile[:age] = "26"
+profile[:age] = 26
 profile[:address] = "高知"
-puts profile
 
 profiles = []
 profiles << profile
 
 profile2 = {}
 profile2[:name] = "二郎"
-profile2[:age] = "27"
+profile2[:age] = 27
 profile2[:address] = "愛媛"
-puts profile2
 
 profiles << profile2
 
-puts profiles
+# puts profiles
+
+
+def withdraw(balance, amount)
+  fee = 110  # 手数料
+# 引き落とし額と残高を表示する、もしくは残高より多く引き落としたら残高不足と表示
+  total = amount + fee
+  if balance >= total
+    balance -= total
+    puts "#{amount}円引き落としました。残高は#{balance}円です"
+  else
+    puts "残高不足です"
+  end
+end
+
+balance = 100000  # 残高
+puts "いくら引き落としますか？（手数料110円かかります）"
+money = gets.to_i
+withdraw(balance, money)
